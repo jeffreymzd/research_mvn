a5f27d1569ddb62e47cc0e14f59cc24881c4301e
Jeffrey Mo <mzhde@hotmail.com>
2017-09-23 18:08:45 -0400

    Factorial test

--- src/main/java/com/jeff/research/math/FactorialCalculator.java
+++ src/main/java/com/jeff/research/math/FactorialCalculator.java
@@ -1,11 +1,14 @@
 package com.jeff.research.math;
 
+import java.math.BigInteger;
+
 public class FactorialCalculator {
-    public static int factorial(int input) {
+    public static BigInteger factorial(int input) {
+        BigInteger bi = BigInteger.valueOf(new Long(input));
         if (input == 0 || input == 1) {
-            return 1;
+            return BigInteger.valueOf(1);
         } else {
-            return input * factorial(--input);
+            return bi.multiply(factorial(input-1));
         }
     }
 }
--- src/test/java/com/jeff/research/math/FactorialCalculatorTest.java
+++ src/test/java/com/jeff/research/math/FactorialCalculatorTest.java
@@ -7,10 +7,9 @@ import static org.junit.Assert.*;
 public class FactorialCalculatorTest {
     @Test
     public void factorial() throws Exception {
-        assertEquals(1, FactorialCalculator.factorial(1));
-        assertEquals(2, FactorialCalculator.factorial(2));
-        assertEquals(6, FactorialCalculator.factorial(3));
-        assertEquals(24, FactorialCalculator.factorial(4));
-        System.out.println(FactorialCalculator.factorial(20));
+        assertEquals(1, FactorialCalculator.factorial(1).longValue());
+        assertEquals(2, FactorialCalculator.factorial(2).longValue());
+        assertEquals(6, FactorialCalculator.factorial(3).longValue());
+        assertEquals(24, FactorialCalculator.factorial(4).longValue());
     }
 }
